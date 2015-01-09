96320afd136732d31ea6c9510967affb210ad6f7
Pyry-Samuli Lahti <pyry-samuli.lahti@onomatics.com>
2015-01-09 17:02:44 +0200

    HttpUtils & timeouts

--- src/main/scala/fi/pyppe/subtitler/HttpUtils.scala
+++ src/main/scala/fi/pyppe/subtitler/HttpUtils.scala
@@ -5,13 +5,21 @@ import dispatch._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object HttpUtils {
+  import scala.concurrent.duration._
+
+  implicit class FiniteDurationExtras(duration: FiniteDuration) {
+    def millisInt: Int = duration.toMillis.toInt
+  }
+
+  val http = Http.configure(_.setConnectionTimeoutInMs(20.seconds.millisInt).
+                              setRequestTimeoutInMs(1.minute.millisInt))
 
   def postXML(targetUrl: String, xml: Elem): Future[Elem] = {
     val req =
       url(targetUrl).POST.
         setBody(xml.toString).
         setHeader("Content-Type", "application/xml")
-    Http(req).map { response =>
+    http(req).map { response =>
       require(response.getStatusCode == 200, s"Invalid response code: ${response.getStatusCode}")
       XML.load(response.getResponseBodyAsStream)
     }
